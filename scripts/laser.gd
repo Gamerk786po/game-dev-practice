fa1d4f2205e7049e793347fa806d6021926cb3db
Gamerk786po <abdullahpper@gmail.com>
2026-01-20 12:23:16 +0500

    2d emitter for lasers added

--- scripts/laser.gd
+++ scripts/laser.gd
@@ -5,7 +5,7 @@ var speed = 1000
 var direction:Vector2 = Vector2.UP
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$GPUParticles2D.emitting = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
